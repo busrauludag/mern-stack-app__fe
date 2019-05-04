dd4276601e6892efe04af77af903c20963ca2c2b
busrauludag <busrauludag13@gmail.com>
2019-05-04 18:40:02 +0300

    route added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min';
 
 class App extends Component {
-  return (
-    
-  );
+  render(){
+    return (
+      <Router>
+        <div className="container">
+          <h2>MERN - Stack To Do App</h2>
+        </div>
+        <Route path="/" exact component="{TodosList}" />
+        <Route path="/edit/:id" component="{EditTodo}" />
+        <Route path="/create" component="{CreateTodo}" />
+      </Router>
+    );
+  }
 }
 
 export default App;
